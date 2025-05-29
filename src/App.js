caffc013e96d8a8fb7c2932660dc3363d1b165af
darkmine95 <abenbrikho@gmail.com>
2025-05-29 14:58:56 +0200

    Add licences in menu left

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import React, { useEffect, useState } from 'react';
 import Content from './components/Content/Content';
 import MenuLeft from './components/MenuLeft/MenuLeft';
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/licences.json')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Erreur lors du chargement du fichier JSON');
+        }
+        return response.json();
+      })
+      .then((jsonData) => setData(jsonData))
+      .catch((error) => console.error('Erreur:', error));
+  }, []);
+
   return (
     <div className="app">
       <Navbar />
-      <MenuLeft />
+      <MenuLeft data={data} />
       <Content />
     </div>
   );
--- src/components/MenuLeft/MenuLeft.jsx
+++ src/components/MenuLeft/MenuLeft.jsx
@@ -1,9 +1,45 @@
+import React, { useMemo } from 'react';
 import './MenuLeft.css';
 
-function MenuLeft() {
+function MenuLeft({ data }) {
+  // Regroupement des licences par nom
+  const dataFormatted = useMemo(() => {
+    let dataFormattedTmp = {};
+    data.forEach(d => {
+      if (!dataFormattedTmp[d.name]) {
+        dataFormattedTmp[d.name] = [d];
+      } else {
+        dataFormattedTmp[d.name].push(d);
+      }
+    });
+    return dataFormattedTmp;
+  }, [data]);
+
   return (
     <div className="menu-left">
-      menu-left
+      <ul>
+        {
+          Object.keys(dataFormatted).map((licence, index) => {
+            return <>
+              <h2>{licence}</h2>
+              <ul>
+                {
+                  dataFormatted[licence].map((elt) =>
+                    <li>
+                      <div className='licence-icon'>
+                        <img src={'/icons/' + elt.icon} alt='licence-icon' />
+                      </div>
+                      <div className='licence-name'>
+                        {elt.version}
+                      </div>
+                    </li>
+                  )
+                }
+              </ul>
+            </>
+          })
+        }
+      </ul>
     </div>
   );
 }
